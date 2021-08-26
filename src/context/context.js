91178fc31886afad2df35fbd7544253f970e2fdb
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-08-26 11:07:25 +0530

    context Update

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
+import { Children } from "react";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
+
+const GithubContext = React.createContext();
+
+const GithubProvider = ({ children }) => {
+  return (
+    <GithubContext.Provider value={"karthik"}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
+export { GithubContext, GithubProvider };
