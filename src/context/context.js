e6570baadc74479aec40d56cb927da97b6a9f086
karthik selvam <41238125+karthikashutosh@users.noreply.github.com>
2021-08-26 11:42:32 +0530

    Added mockUser

--- src/context/context.js
+++ src/context/context.js
@@ -10,8 +10,11 @@ const rootUrl = "https://api.github.com";
 const GithubContext = React.createContext();
 
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
   return (
-    <GithubContext.Provider value={"karthik"}>
+    <GithubContext.Provider value={githubUser,repos,followers}>
       {children}
     </GithubContext.Provider>
   );
